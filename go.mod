679aef351eaa92f21c19ac06f4b922e398b4bddb
Rakesh Mothukuri <rockey5520@gmail.com>
2021-03-24 21:08:55 +0100

    lesson 2.1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module get-programming-with-go
+
+go 1.15
---
+++ lesson-2/mars/mars.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("My weight on the surface of mars is ")
+	fmt.Print(149.0 * 0.3783)
+	fmt.Print(" lbs, and I would be ")
+	fmt.Print(41 * 365 / 687)
+	fmt.Print(" years old.")
+}
